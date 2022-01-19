19f8247c57979bd3aa4d29407237066ee611fa75
Tina Thompson <mark_junior13@yahoo.com>
2022-01-19 09:41:54 -0600

    add very basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dumpster", password: 'milkshake', except: [:index, :show]
+
   def index
     @articles = Article.all
   end
